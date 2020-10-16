dba4217aee6cf50c16b294e75c85d9154c4ad01c
JesperMell <jesper.mellquist@hotmail.com>
2020-10-16 12:14:07 +0200

    update: deploy script again

--- run.sh
+++ run.sh
@@ -6,23 +6,15 @@
 # @created     : fredag okt 16, 2020 11:50:04 CEST
 #
 # @description : A temporary solution to deploy the application to 
-#	       : production environment.
+#              : production environment.
 ######################################################################
 
 DOCKER_CONTAINER=1b18
 
-RAILS_PROD_CMD='RAILS_ENV=production SECRET_KEY_BASE=production_key'
-
-docker restart $DOCKER_CONTAINER
-docker start $DOCKER_CONTAINER
-
-git pull
-
-docker exec -w /app -d $DOCKER_CONTAINER /bin/sh -c '
-  export APP_DATABASE_PASSWORD=secret &&\n
-  $RAILS_PROD_CMD rake db:migrate assets:precompile &&\n
-  $RAILS_PROD_CMD rails s -b 0.0.0.0 -p 80
-'
-
-
+docker restart $DOCKER_CONTAINER &&
+        docker start $DOCKER_CONTAINER &&
+	git pull &&
+        docker exec -d -w /app $DOCKER_CONTAINER /bin/sh -c 'yarn install --check-files && export APP_DATABASE_PASSWORD=secret &&\n
+  RAILS_ENV=production SECRET_KEY_BASE=production_key rake db:migrate assets:precompile &&\n
+  RAILS_ENV=production SECRET_KEY_BASE=production_key rails s -b 0.0.0.0 -p 80
 
